fd22ef424b048e4ca47ac2694fa77a4600446bcf
MarcusM <mmoldaschel@lulzbot.com>
2020-06-08 08:57:17 -0500

    Updating to Marlin 2.0.5.3

--- Marlin/Marlin.ino
+++ Marlin/Marlin.ino
@@ -3,7 +3,7 @@
 
   Marlin Firmware
 
-  (c) 2011-2018 MarlinFirmware
+  (c) 2011-2019 MarlinFirmware
   Portions of Marlin are (c) by their respective authors.
   All code complies with GPLv2 and/or GPLv3
 
@@ -12,8 +12,8 @@
 Greetings! Thank you for choosing Marlin 2 as your 3D printer firmware.
 
 To configure Marlin you must edit Configuration.h and Configuration_adv.h
-located in the root 'Marlin' folder. Check the example_configurations folder to
-see if there's a more suitable starting-point for your specific hardware.
+located in the root 'Marlin' folder. Check the config/examples folder to see if
+there's a more suitable starting-point for your specific hardware.
 
 Before diving in, we recommend the following essential links:
 
