228fcbdcf600d8af4dee67787129d2fc4cfde733
bmkahl <bkahl@lulzbot.com>
2022-06-29 10:34:13 -0500

    adding Park option in LCD menu

--- Marlin/src/lcd/menu/menu_motion.cpp
+++ Marlin/src/lcd/menu/menu_motion.cpp
@@ -429,6 +429,13 @@ void menu_motion() {
   #endif
 
   //
+  // Park Nozzle
+  //
+  #if ENABLED(PARK_NOZZLE_MENU_OPTION)
+    GCODES_ITEM(MSG_IDEX_MODE_AUTOPARK, PSTR("G28 O\nG27"));
+  #endif
+
+  //
   // Auto-calibration
   //
   #if ENABLED(CALIBRATION_GCODE)
