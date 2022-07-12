b40b6b60d7e98c391f34deff18efd4fab68eab08
bmkahl <bkahl@lulzbot.com>
2022-07-12 11:14:44 -0500

    spacing out tool heads menu

--- Marlin/src/lcd/extui/ftdi_eve_touch_ui/generic/custom_user_menus.cpp
+++ Marlin/src/lcd/extui/ftdi_eve_touch_ui/generic/custom_user_menus.cpp
@@ -55,7 +55,7 @@ void CustomUserMenus::onRedraw(draw_mode_t what) {
 
   #if ENABLED(TOUCH_UI_PORTRAIT)
     #define GRID_COLS (1 + _MORE_THAN_TEN)
-    #define GRID_ROWS 11
+    #define GRID_ROWS 7
     #define USER_ITEM_POS(N) BTN_POS((1+((N-1)/10)), ((N-1) % 10 + 1)), BTN_SIZE(1,1)
     #define BACK_POS         BTN_POS(1,11), BTN_SIZE(1,1)
   #else
